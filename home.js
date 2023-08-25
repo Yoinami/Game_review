2c79fff95b472c5b1547e7375cfb91fc8c200cde
Yoinami <thihansoe912@gmail.com>
2023-08-25 09:52:47 +0630

    commit

--- home.js
+++ home.js
@@ -16,18 +16,26 @@ function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
+    let box = document.getElementsByClassName("slide-moreinfo");
+
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
+
     for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+        slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace(" active", "");
     }
+    for(i=0; i < box.length; i++) {
+        box[i].style.display = "none";
+    }
+
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
+    box[slideIndex-1].style.display = "block";
 }
-setInterval(plusSlides, 8000);
+//setInterval(plusSlides, 8000);
 function showText() {
     let text = document.getElementsByClassName("caption");
     let i;
